0b530104be7157cac1fec106d379d6da6394b199
ZQ <zengqiang_1988@163.com>
2014-09-21 16:37:34 +0800

    主要是为了nginx转发问题

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 
 define('PHPCMS_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
+if (strpos($_SERVER['REQUEST_URI'], 'phpsso_server')!==false) {
+    include_once PHPCMS_PATH.'/phpsso_server/index.php';
+    exit;
+}
+
 include PHPCMS_PATH.'/phpcms/base.php';
 
 pc_base::creat_app();
